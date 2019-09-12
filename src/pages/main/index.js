56bfcc952dbe2ad8f2f0483af8fee9f6d1624e68
Henrique Negri <henrique@aiqfome.com>
2019-09-12 17:12:36 -0300

    configurando  busca em api

---
+++ src/pages/main/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import api from '../../services/api.js';
+
+export default class Main extends Component {
+    state = {
+        products: [],
+    }
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+        
+        this.setState({ products: response.data.docs })
+    };
+
+    render() {
+        return (
+            <div className="product-list">
+                {this.state.products.map(products => (
+                    <h2 key={products._id} > { products.title } </h2>
+                ))};
+            </div>
+        );
+    }
+
+}
